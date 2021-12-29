a0cfb9afb36b7d84faaead7ca855cc3935f8f7e3
Adetunji Adeyinka <adetunjiadeyinka29@gmail.com>
2021-12-29 17:58:09 +0100

    Created Piece.jsx, BoardSquare.jsx and Square.jsx

---
+++ src/BoardSquare.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Square from './Square'
+
+export default function BoardSquare({piece}) {
+  return (
+    <div>
+      <Square>
+
+      </Square>
+    </div>
+  )
+}
