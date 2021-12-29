fe30117716d04f959f296209835534052fd00210
Adetunji Adeyinka <adetunjiadeyinka29@gmail.com>
2021-12-29 17:59:37 +0100

    Created Piece.jsx, BoardSquare.jsx and Square.jsx

--- src/BoardSquare.jsx
+++ src/BoardSquare.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Square from './Square'
+import Piece from './Piece'
 
 export default function BoardSquare({piece}) {
   return (
     <div>
       <Square>
-
+        <Piece piece={piece}/>
       </Square>
     </div>
   )
