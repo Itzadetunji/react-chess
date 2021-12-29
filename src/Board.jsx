afe425f9eb97a29d93c3d1e401c3db0384eafc1a
Adetunji Adeyinka <adetunjiadeyinka29@gmail.com>
2021-12-29 18:00:46 +0100

    Made the array 2-Dimensional

--- src/Board.jsx
+++ src/Board.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Board({board}) {
   return <div className="board">
-    {board.map((piece, i) => (
+    {board.flat().map((piece, i) => (
       <div key={i} className="square">
         <p>{JSON.stringify(piece)}</p>
       </div>
