6bcc3dfed90d54f232d27a8ad7004d1a48a758fe
Adetunji Adeyinka <adetunjiadeyinka29@gmail.com>
2021-12-30 19:31:02 +0100

    Created Promote component and listened if there was a promotion so the component can be rendered

---
+++ src/Promote.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const promotionPieces = ['r', 'n', 'b', 'q']
+
+export default function Promote({
+  promotion: {from,to,color},
+}) {
+
+  return (
+    <div className="board">
+      {promotionPieces.map((p, i) => (
+        <div key={i} className="promote-square">
+          {p}
+        </div>
+      ))}
+    </div>
+  )
+}
